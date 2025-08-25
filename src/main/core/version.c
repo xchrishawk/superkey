4cd94fb62d7cc3613c89bb3bf25816d7cf1a879c
Chris Vig <christopher.vig@garmin.com>
2025-08-25 14:49:17 -0500

    Make version number configurable through CMake.

--- src/main/core/version.c
+++ src/main/core/version.c
@@ -14,35 +14,17 @@
 
 /* --------------------------------------------------- CONSTANTS ---------------------------------------------------- */
 
-/**
- * @def     VERSION_MAJOR
- * @brief   The application's current major version number.
- */
-#define VERSION_MAJOR    0
-
-/**
- * @def     VERSION_MINOR
- * @brief   The application's current minor version number.
- */
-#define VERSION_MINOR    1
-
-/**
- * @def     VERSION_REVISION
- * @brief   The application's current revision version number.
- */
-#define VERSION_REVISION 0
-
 // Public variables
-uint8_t const PRODUCT_VERSION_MAJOR = VERSION_MAJOR;
-uint8_t const PRODUCT_VERSION_MINOR = VERSION_MINOR;
-uint8_t const PRODUCT_VERSION_REVISION = VERSION_REVISION;
+uint8_t const PRODUCT_VERSION_MAJOR = _VERSION_MAJOR;
+uint8_t const PRODUCT_VERSION_MINOR = _VERSION_MINOR;
+uint8_t const PRODUCT_VERSION_REVISION = _VERSION_REVISION;
 
 // Auto-generated variables
 static char const * const PRODUCT_NAME = _EXECUTABLE_PROJECT_NAME;
 static char const * const EXECUTABLE_NAME = _EXECUTABLE_NAME;
-static char const * const VERSION = stringize_value( VERSION_MAJOR ) "."
-                                    stringize_value( VERSION_MINOR ) "."
-                                    stringize_value( VERSION_REVISION );
+static char const * const VERSION = stringize_value( _VERSION_MAJOR ) "."
+                                    stringize_value( _VERSION_MINOR ) "."
+                                    stringize_value( _VERSION_REVISION );
 static char const * const BUILD_TYPE = _BUILD_TYPE;
 static char const * const BUILD_DIR = _EXECUTABLE_PROJECT_BUILD_DIR;
 static char const * const BUILD_DATE = _EXECUTABLE_BUILD_DATE;
@@ -58,9 +40,9 @@ void version_get( version_t * version )
     version->product_name       = PRODUCT_NAME;
     version->executable_name    = EXECUTABLE_NAME;
     version->version            = VERSION;
-    version->version_major      = VERSION_MAJOR;
-    version->version_minor      = VERSION_MINOR;
-    version->version_revision   = VERSION_REVISION;
+    version->version_major      = PRODUCT_VERSION_MAJOR;
+    version->version_minor      = PRODUCT_VERSION_MINOR;
+    version->version_revision   = PRODUCT_VERSION_REVISION;
     version->build_type         = BUILD_TYPE;
     version->build_dir          = BUILD_DIR;
     version->build_date         = BUILD_DATE;
