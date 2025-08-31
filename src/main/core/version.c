8bd24a20daabd6ef7e1c2e54017c3dba75796a2b
Chris Vig <chris@invictus.so>
2025-08-31 11:37:29 -0500

    Bump version to 0.1.2.

--- src/main/core/version.c
+++ src/main/core/version.c
@@ -31,7 +31,7 @@
  * @def     VERSION_REVISION
  * @brief   The application's revision version number.
  */
-#define VERSION_REVISION            1
+#define VERSION_REVISION            2
 
 // Public variables
 uint8_t const PRODUCT_VERSION_MAJOR = VERSION_MAJOR;
